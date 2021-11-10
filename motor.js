b12d1781171af9ba83c71ffab124ef415a91d388
Milton Jose Ribeiro Neto <88862184+miltonrt@users.noreply.github.com>
2021-11-10 16:59:32 -0300

    Add files via upload

---
+++ motor.js
@@ -0,0 +1,15 @@
+function	alrtin(){
+	alert ("Gostaria de receber em primeira mão nossas novidades e promoções?")
+}
+
+function	alrtlg(){
+	alert ("Login realizado com sucesso!")
+}
+
+function	alrtnu(){
+	alert ("Seu cadastro foi realizado com sucesso!")
+}
+
+function	alrter(){
+	alert ("Desculpe! Parece que houve um problema em nosso sistema.")
+}
